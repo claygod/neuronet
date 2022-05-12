089c3f6cd3159a634222c5b5fbe5d2d35763a4dc
Eduard Sesigin <evsesigin@mts.ru>
2022-05-12 19:30:03 +0300

    problem-based approach

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/claygod/neuronet
+
+go 1.16
+
+require (
+)
---
+++ problem-based-approach/entities.go
@@ -0,0 +1,25 @@
+package problembasedapproach
+
+// Problem-based approach
+// Copyright © 2022 Eduard Sesigin. All rights reserved. Contacts: <claygod@yandex.ru>
+
+/*
+Need - потребность
+*/
+type Need struct {
+	ID string
+}
+
+type TaskGenerator interface {
+	GenTaskFromNeed(*Need)
+}
+
+/*
+Task - задача
+*/
+type Task struct {
+	ID          string
+	State       uint64 // состояние, возможно будет каждый раз пересчитываться (получать через метод)
+	ParentTasks []*Task
+	ChildTasks  []*Task
+}
